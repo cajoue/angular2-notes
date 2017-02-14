9ff7e640b58ae4b022409d622642e89b9326ed87
cajoue <cajoue@users.noreply.github.com>
2017-02-14 14:29:40 +0100

    feat: Add data object for note-creator
    So that new notes can be added to app

--- src/app/ui/note-creator/note-creator.component.ts
+++ src/app/ui/note-creator/note-creator.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCreatorComponent implements OnInit {
 
+  newNote = {
+    title: '',
+    value: ''
+  }
+
   constructor() { }
 
   ngOnInit() {
