4ef3847ee0a62731f8155b25eb9709a2672f0690
cajoue <cajoue@users.noreply.github.com>
2017-02-17 13:53:23 +0100

    feat: Integrate notes service into component
    Use API and Notes services to replace static note create/delete methods

--- src/app/containers/notes/notes.component.ts
+++ src/app/containers/notes/notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../../services/notes.service';
 
 @Component({
   selector: 'app-notes',
@@ -6,22 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
-  notes = [
+  /*notes = [
     { title: 'House move', value: 'pack boxes', color: 'lightblue' },
     { title: 'Exercise', value: 'HIIT training', color: 'orange' },
     { title: 'Dogs', value: 'Look for adoptable dogs', color: 'gold' }
-  ];
+  ];*/
 
-  constructor() { }
+  notes = [];
+
+  constructor(private noteService: NotesService) { 
+    this.noteService.getNotes()
+                    .subscribe(resp => this.notes = resp.data);
+  }
 
   ngOnInit() {
   }
 
-  onNoteChecked(i: number){
+  /*onNoteChecked(i: number){
     this.notes.splice(i, 1);
+  }*/
+
+  onNoteChecked(note) {
+    this.noteService.completeNote(note)
+                    .subscribe(note => {
+                      const i = this.notes.findIndex(localNote => localNote.id === note.id);
+                      this.notes.splice(i, 1);                  
+                    })
   }
 
   onCreateNote(note) {
-    this.notes.push(note);
+    /*this.notes.push(note);*/ 
+    this.noteService.createNote(note)
+                    .subscribe(note => this.notes.push(note));
+
   }
 }
